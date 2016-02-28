faf04bce515ae3b709270c485a603b42fdfc377e
Swooshed <dmcgill9071@gmail.com>
2016-02-28 14:53:00 +0000

    second setup commit

---
+++ carved_unity/Assets/NewBehaviourScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("Hello world!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
