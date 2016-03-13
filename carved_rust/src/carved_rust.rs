49f81d65ebc1c81ab9b72817b92bd5773e00cc25
David McGillicuddy <david.mcgillicuddy@moneyfarm.com>
2016-03-13 21:06:26 +0000

    reduce number of mut pointers

--- carved_rust/src/carved_rust.rs
+++ carved_rust/src/carved_rust.rs
@@ -30,7 +30,7 @@ pub extern "stdcall" fn svo_destroy(svo_ptr: *mut SVO) {
 }
 
 #[no_mangle]
-pub extern "stdcall" fn svo_on_voxels(svo_ptr: *mut SVO, on_voxel: fn(Vec3<f32>, i32, i32) -> ()) {
+pub extern "stdcall" fn svo_on_voxels(svo_ptr: *const SVO, on_voxel: fn(Vec3<f32>, i32, i32) -> ()) {
 	let svo_ref: &SVO = unsafe { &*svo_ptr };
 	svo_ref.on_voxels(on_voxel);
 }
@@ -42,7 +42,7 @@ pub struct BadOption<T : Sized> {
 }
 
 #[no_mangle]
-pub extern "stdcall" fn svo_cast_ray(svo_ptr: *mut SVO, ray_origin: Vec3<f32>, ray_dir: Vec3<f32>) -> BadOption<Vec3<f32>> {
+pub extern "stdcall" fn svo_cast_ray(svo_ptr: *const SVO, ray_origin: Vec3<f32>, ray_dir: Vec3<f32>) -> BadOption<Vec3<f32>> {
 	let svo_ref: &SVO = unsafe { &*svo_ptr };
 	let maybe_hit = svo_ref.cast_ray(ray_origin, ray_dir);
 	BadOption {
