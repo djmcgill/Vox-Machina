80c1044cce2fc88b5639c5fecdd9569a220158bf
David McGillicuddy <david.mcgillicuddy@moneyfarm.com>
2016-03-14 20:47:26 +0000

    Added C# OnVoxels

--- carved_unity/Assets/SVO.cs
+++ carved_unity/Assets/SVO.cs
@@ -29,6 +29,7 @@ public class SVO : IDisposable
 		if (disposed)
 			return;
 		svo_destroy (svoPtr);
+		disposed = true;
 	}
 
 	~SVO()
@@ -38,8 +39,17 @@ public class SVO : IDisposable
 
 	public delegate void OnBlocksCallback(Vector3 vec, int depth, int voxelType);
 
+	public void OnBlocks (OnBlocksCallback onBlocks)
+	{
+		RustOnBlocksCallback rustOnBlocks = (Vec3 vec, int depth, int voxelType) => {
+			var vector = new Vector3 (vec.x, vec.y, vec.z);
+			onBlocks (vector, depth, voxelType);
+		};
+		svo_on_voxels(svoPtr, rustOnBlocks);
+	}
+
 	/// Will return null if the ray misses.
-	public Nullable<Vector3> CastRay(Vector3 rayOrigin, Vector3 rayDirection)
+	public Nullable<Vector3> CastRay (Vector3 rayOrigin, Vector3 rayDirection)
 	{
 		var rustOrigin = new Vec3 (rayOrigin.x, rayOrigin.y, rayOrigin.z);
 		var rustDir = new Vec3 (rayDirection.x, rayDirection.y, rayDirection.z);
@@ -58,22 +68,22 @@ public class SVO : IDisposable
 
 	// FFI interface
 	[DllImport("libcarved_rust")]
-	private static extern IntPtr svo_create(int x);
+	private static extern IntPtr svo_create (int x);
 
 	[DllImport("libcarved_rust")]
-	private static extern void svo_destroy(IntPtr svo);
+	private static extern void svo_destroy (IntPtr svo);
 
-	private delegate void RustOnBlocksCallback(Vec3 vec, int depth, int voxel_type);
+	private delegate void RustOnBlocksCallback (Vec3 vec, int depth, int voxel_type);
 
 	[DllImport("libcarved_rust")]
-	private static extern void svo_on_voxels(IntPtr svo, RustOnBlocksCallback onBlocks);
+	private static extern void svo_on_voxels (IntPtr svo, RustOnBlocksCallback onBlocks);
 
 	[StructLayout(LayoutKind.Sequential)]
 	private struct Vec3
 	{
 		public float x, y, z;
 
-		public Vec3(float _x, float _y, float _z)
+		public Vec3 (float _x, float _y, float _z)
 		{
 			x = _x;
 			y = _y;
@@ -89,6 +99,6 @@ public class SVO : IDisposable
 	}
 
 	[DllImport("libcarved_rust")]
-	private static extern BadOptionVec3 svo_cast_ray(IntPtr svo, Vec3 rayOrigin, Vec3 rayDir);
+	private static extern BadOptionVec3 svo_cast_ray (IntPtr svo, Vec3 rayOrigin, Vec3 rayDir);
 }
 
